249908707ba01aea6dd9e6458f96fd2adc415889
ggabbylopez <gml0426@email.unc.edu>
2023-05-06 17:13:01 -0400

    Add scripts for page organization

--- public/scripts.js
+++ public/scripts.js
@@ -2,7 +2,39 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
+import { rps } from "../lib/rpsls";
+
 //showing shot options based off whether there is an opponent
 function shotView(){
-    
+    const checker = document.getElementById('opponent');
+    if (checker.checked){
+       //if true check which game is chosen
+        checker = document.getElementById('rps');
+        if(checker.checked){
+            $('.rps').show();
+            $('.rpsls').hide();
+        } else {
+            $('.shot').show();
+        }
+    }
+}
+
+function showResults(){
+    $('results').show();
+    document.getElementById('gamePlay').hidden=true;
+}
+function hideResults(){
+    $('results').hide();
+    document.getElementById('gamePlay').hidden=false
+}
+function showRules(r){
+    if (r){
+        $('rules').show();
+        document.getElementById('show').hidden = true;
+        document.getElementById('hide').hidden = false;
+    } else {
+        $('rules').hide();
+        document.getElementById('show').hidden = false;
+        document.getElementById('hide').hidden = true;
+    }
 }
